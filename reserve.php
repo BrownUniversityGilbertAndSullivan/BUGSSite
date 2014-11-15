6f64c39203ad0e7fd487b8dc29117838a8232b6b
zzagorsk <zzagorsk@cs.brown.edu>
2014-11-14 19:00:15 -0500

    Added reserve.php so old reserve links will work

---
+++ reserve.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/commonstyle.css" type="text/css"/>
+        <link rel="stylesheet" href="css/homestyle.css" type="text/css"/>
+        <link rel="icon" type="image/png" href="graphics/favicon.png">
+
+        <title>Brown University Gilbert & Sullivan</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Brown University Gilbert & Sullivan">
+        
+        <!--OpenGraph/Facebook Support -->
+        <meta property="og:title" content="Brown University Gilbert & Sullivan" />
+        <meta property="og:site_name" content="students.brown.edu" />
+        <meta property="og:url" content="http://students.brown.edu/BUGS/" />
+        <meta property="og:description" content="Brown University Gilbert and Sullivan (BUGS) 
+              is an entirely student-run group devoted to producing two high-quality shows each year, 
+              from the works of Gilbert and Sullivan (or works thus inspired), 
+              for the enjoyment of the Brown community" />
+        <meta property="og:image" content="graphics/bugs_logo.png" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="224275021050338" />
+        
+    </head>
+    <body>
+        <div class="graphic" id="backdrop">
+            <div class="menu-container">
+                <div class="content-box" id="center-main">
+                    
+                    <button id="exit">X</button>
+                    <div id="text">
+                        <!--Center content goes here. -->
+                        <h2>Ticket Reservations</h2>
+						<h3>Ruddigore, November 14-16</h3>
+						<p class="topmain">Performances will be held Friday at 8pm, Saturday at 2pm and 8pm, and Sunday at 3pm in Alumnae Hall (194 Meeting Street).</p>
+
+						<!-- GOOGLE DOC FOR RESERVATIONS : EDIT BELOW -->
+						
+						<iframe src="https://docs.google.com/forms/d/1-rQPWSp2zKhM6nbkwMv4xtwbY66WOgyoRuYw85qu_hE/viewform?embedded=true" width="550" height="700" frameborder="0" marginheight="0" marginwidth="0"></iframe>
+						<!-- GOOGLE DOC FOR RESERVATIONS : EDIT ABOVE -->
+
+                        
+<!--                        <h2>
+                        BUGS is proud to announce our
+                        Fall 2014 production of <em>Ruddigore</em>!
+                        </h2>
+                        <h3>
+                            Directed by Jonathan West ‘16<br>
+                        Music Directed by Ian MacDougald '17
+                        and Jonny Abrams ‘15
+                        </h3>
+                        <h3>
+                        E-mail Production Manager Solomon Goldstein-Rose
+                        (Solomon_Goldstein-Rose@brown.edu) if you are
+                        interested in being part of the production team
+                        </h3>-->
+                        <!--Center content ends here. -->
+                    </div>
+                </div>
+                <div class="content-box" id="box-tl">
+                    
+                </div>
+                <div class="content-box" id="box-ml">
+                    
+                </div>
+                <div class="content-box" id="box-bl">
+                    
+                </div>
+                <div class="content-box" id="box-tr">
+                    
+                </div>
+                <div class="content-box" id="box-mr">
+                    
+                </div>
+                <div class="content-box" id="box-br">
+                    <div class="fb-like-box" data-href="https://www.facebook.com/BrownUniversityGandS" data-width="250" data-height="175" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="true" data-show-border="false"></div>
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                      var js, fjs = d.getElementsByTagName(s)[0];
+                      if (d.getElementById(id)) return;
+                      js = d.createElement(s); js.id = id;
+                      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=156977541062010&version=v2.0";
+                      fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));</script>
+                </div>
+                <div class="link-box" id="link-bl">
+                    <a href="http://www.brown.edu" target="_blank">
+                        Brown University</a>
+                </div>
+                <div class="link-box" id="link-bm">
+                    <a href="http://www.brown.edu/Administration/Student_Activities" target="_blank">
+                        Student Activities Office</a>
+                </div>
+                <div class="link-box" id="link-br">
+                    <a href="http://www.brown.edu/Facilities/Facilities_Management/maps.php" target="_blank">
+                        Campus Maps</a>
+                </div>
+                <a class="home-link" href="index.html"></a>
+                <a class="menu-link" id="menu1" href="aboutbugs.html"></a>
+                <a class="menu-link" id="menu2" href="aboutgs.html"></a>
+                <a class="menu-link" id="menu3" href="links.html"></a>
+                <a class="menu-link" id="menu4" href="gallery.html"></a>
+                <a class="menu-link" id="menu5" href="past_productions/camelot.html"></a>
+                <a class="menu-link" id="menu6" href="getinvolved.html"></a>
+            </div>
+        </div>
+        <script src="js/donate.js"></script>
+    </body>
+</html>
